47f3d5ae89e057bff457a5f0fffc5ac315246981
Siddhesh Surve <sysurve24@gmail.com>
2024-01-14 05:41:49 +0000

    sidebar and bg img

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ from PIL import Image
 
 model = pickle.load(open('model.sav', 'rb'))
 
+st.title('Player Salary Predictor')
+st.sidebar.header('Player Data')
+image = Image.open('bg.jpg')
+st.image(image, '')
